moltam89/liferay-portal

LPS-72930 LAR export does not include new pages if existing export task is re-launched

modules/apps/web-experience/export-import/export-import-service/src/main/java/com/liferay/exportimport/controller/LayoutExportController.java
@@ -409,6 +409,12 @@ public class LayoutExportController implements ExportController {
 			settingsMap, "privateLayout");
 		long[] layoutIds = GetterUtil.getLongValues(
 			settingsMap.get("layoutIds"));
+		boolean allPages = MapUtil.getBoolean(settingsMap, "allPages");
+
+		if (allPages) {
+			layoutIds = ExportImportHelperUtil.getAllLayoutIds(
+				sourceGroupId, privateLayout);
+		}
 
 		portletDataContext.setExportImportProcessId(
 			String.valueOf(

FaisalNkutu/com-liferay-export-import

export-import-service/src/main/java/com/liferay/exportimport/controller/LayoutExportController.java
@@ -420,6 +420,12 @@ public class LayoutExportController implements ExportController {
 			settingsMap, "privateLayout");
 		long[] layoutIds = GetterUtil.getLongValues(
 			settingsMap.get("layoutIds"));
+		boolean allPages = MapUtil.getBoolean(settingsMap, "allPages");
+
+		if (allPages) {
+			layoutIds = _exportImportHelper.getAllLayoutIds(
+				sourceGroupId, privateLayout);
+		}
 
 		portletDataContext.setExportImportProcessId(
 			String.valueOf(